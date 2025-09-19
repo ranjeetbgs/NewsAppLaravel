985b84f17d9ff44fa9b40c29e37dfe2ce1702961
Ranjeet Kumar <ranjitbeg2009@gmail.com>
2025-09-19 16:37:55 +0530

    update send notification

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('admin/layout/app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Logina') }}</div>
+                <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
